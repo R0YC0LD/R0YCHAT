1627b5c645ec2db75527b9d953af9c3ebccbae31
R0YC0LD <77665809+R0YC0LD@users.noreply.github.com>
2025-05-14 21:44:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -35,3 +35,50 @@ const port = 3000;
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+let onlineUsers = [];  // Aktif kullanıcıları tutacak
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  
+  // Yeni kullanıcı bağlandığında, tüm kullanıcılara listeyi gönder
+  socket.on('login', (username) => {
+    socket.username = username; // Kullanıcı adını sakla
+    onlineUsers.push(username);
+    io.emit('userList', onlineUsers);  // Tüm kullanıcılara listeyi gönder
+  });
+
+  // Kullanıcı bağlantısını keserse, listeyi güncelle
+  socket.on('disconnect', () => {
+    onlineUsers = onlineUsers.filter(user => user !== socket.username);
+    io.emit('userList', onlineUsers);
+    console.log('User disconnected');
+  });
+
+  // Mesaj gönderme işlemi
+  socket.on('message', async ({ to, message }) => {
+    // Veritabanına kaydet
+    const newMessage = new Message({ from: socket.username, to, message });
+    await newMessage.save();
+
+    // Mesajı diğer kullanıcılara ilet
+    const recipientSocket = getUserSocketByUsername(to);
+    if (recipientSocket) {
+      recipientSocket.emit('message', { from: socket.username, message });
+    }
+  });
+
+  // Kullanıcıdan alınan mesajları veritabanına kaydet
+  function getUserSocketByUsername(username) {
+    return Object.values(io.sockets.sockets).find(socket => socket.username === username);
+  }
+
+  // Kullanıcı bağlandığında sohbet geçmişini gönder
+  socket.on('login', (username) => {
+    Message.find({ $or: [{ from: username }, { to: username }] })
+      .sort({ timestamp: 1 })
+      .exec((err, messages) => {
+        if (err) console.log(err);
+        socket.emit('chatHistory', messages);
+      });
+  });
+});
